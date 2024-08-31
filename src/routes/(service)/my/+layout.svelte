1ba13841da182b6a3c6db63e349f1202cd397d8c
권세건 <39672943+honeybugs@users.noreply.github.com>
2024-09-01 05:48:15 +0900

    add my page template

---
+++ src/routes/(service)/my/+layout.svelte
@@ -0,0 +1,41 @@
+<script>
+	import { page } from '$app/stores';
+	import { goto } from '$app/navigation';
+	import SettingIcon from '$lib/icons/SettingIcon.svelte';
+	let profile_image = '' ? profile_image : '/profile.png';
+	let name = '박건민';
+	let current = $page.url.pathname;
+	$: {
+		current = $page.url.pathname;
+	}
+</script>
+
+<div class="flex w-full h-full">
+	<div class="w-[217px] min-h-screen h-full flex flex-col items-center border-r border-[#C9C9C9]">
+		<div class="w-[179px] flex items-center justify-between my-[14px]">
+			<div class="flex space-x-[5px] items-center">
+				<img class="w-[36px] h-[36px] rounded-full" alt="profile" src={profile_image} />
+				<p class="text-black text-[24px]">{name}</p>
+			</div>
+			<SettingIcon />
+		</div>
+		<div
+			class="flex flex-col w-full text-[18px] text-black *:w-[206px] *:h-[38px] *:text-left items-center *:px-[14px] *:rounded-[10px] space-y-[1px]"
+		>
+			<button
+				on:click={() => goto('/my/posted')}
+				class={current == '/my/posted' ? 'bg-[#EDEDED]' : 'hover:bg-[#EDEDED]'}>작성글</button
+			>
+			<button
+				on:click={() => goto('/my/purchased')}
+				class={current == '/my/purchased' ? 'bg-[#EDEDED]' : 'hover:bg-[#EDEDED]'}>구매 목록</button
+			>
+			<button
+				on:click={() => goto('/my/bookmarked')}
+				class={current == '/my/bookmarked' ? 'bg-[#EDEDED]' : 'hover:bg-[#EDEDED]'}
+				>저장 목록</button
+			>
+		</div>
+	</div>
+	<div class="w-full h-full flex flex-col items-center py-[45px]"><slot /></div>
+</div>
---
+++ src/routes/(service)/my/posted/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { api } from '$lib/api.js';
+	import { onMount } from 'svelte';
+
+	let posts = [
+		{
+			id: '123',
+			title: '132123',
+			introduce: '!231231'
+		}
+	];
+	onMount(async () => {
+		// posts 에 작성 한 게시물 가져와서 보여주기
+	});
+</script>
