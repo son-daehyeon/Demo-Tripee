9eb33b361daac8ec82e4fc5b06711917e8caf744
권세건 <39672943+honeybugs@users.noreply.github.com>
2024-09-01 03:47:48 +0900

    add search page

---
+++ src/routes/(service)/search/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import SearchIcon from '$lib/icons/SearchIcon.svelte';
+	import ScrollDownIcon from '$lib/icons/ScrollDownIcon.svelte';
+	import LoginView from '$lib/views/LoginView.svelte';
+	import Modal from '$lib/components/Modal.svelte';
+	import PostList from '$lib/components/PostList.svelte';
+
+</script>
+
+<section class="max-w-[1100px] relative w-full h-full flex flex-col items-center justify-center mt-[32px]">
+	<PostList />
+</section>
