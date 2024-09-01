1a421ae1b33df09871f655e1ebfde37ee392a289
Son Daehyeon <sondaehyeon01@gmail.com>
2024-09-01 09:41:34 +0900

    plz

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		proxy: {
-			'/api': {
-				target: 'http://10.223.118.180:8080',
-				changeOrigin: true,
-			},
-		}
-	}
+	plugins: [sveltekit()]
 });
